13dd4df6e2fc379b08e95928ff81fff7bbcbff0a
Carlos Garcia <carlospr.dev@gmail.com>
2025-08-29 02:51:00 +0000

    update avante ai to add new models

--- lua/configs/avante.lua
+++ lua/configs/avante.lua
@@ -1,14 +1,17 @@
 local opts = {
-    -- add any opts here
-    -- for example
-    provider = "openai",
+  -- add any opts here
+  -- for example
+  provider = "openai",
+  providers = {
     openai = {
       endpoint = "https://api.openai.com/v1",
       model = "gpt-4.1-mini",
       timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-      temperature = 0,
-      max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-      --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      extra_request_body = {
+        temperature = 1,
+        max_completion_tokens = 20480,
+      },
     },
   }
+}
 return opts
